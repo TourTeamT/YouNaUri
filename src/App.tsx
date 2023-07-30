a0ce0ae4d343d47162f154c8447fc90a074168b3
murpin <qw04011@gmail.com>
2023-07-30 17:49:35 +0900

    feat: 여행 시간 설정 페이지 추가

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import UserSelect from 'pages/user-select';
 import Place from 'pages/user-select/place';
 import Plan from 'pages/user-select/plan';
 import Partner from 'pages/user-select/partner';
+import Time from 'pages/user-select/time';
 import Login from 'pages/Auth/Login';
 import Map from 'pages/Map';
 import './App.css';
@@ -16,16 +17,16 @@ function App() {
       <div className="App">
         <Routes>
           <Route path="/login" element={<Login />} />
-          <Route path="/dateChoice" element={<DateChoice />} />
           <Route path="/" element={<Header />}>
             <Route path='/user-select' element={<UserSelect />}>
               <Route path='partner' element={<Partner />} />
               <Route path='place' element={<Place />} />
               <Route path='plan' element={<Plan />} />
+              <Route path='time' element={<Time />} />
             </Route>
             <Route path='/myPage' element={<MyPage/>} />
+            <Route path='/map' element={<Map />} />
           </Route>
-          <Route path='/map' element={<Map />} />
         </Routes>
       </div>
     </Router>
