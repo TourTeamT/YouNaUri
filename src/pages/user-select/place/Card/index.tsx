509d022aa4e7e5f253735d780455646b36c1fbb0
김혜준 <alice5855@naver.com>
2023-07-31 14:00:50 +0900

    feat: 스켈레톤 제작

--- src/pages/user-select/place/Card/index.tsx
+++ src/pages/user-select/place/Card/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import styles from './Cards.module.scss';
+import { useState,useEffect } from 'react';
 
 interface Attraction {
     name: string;
@@ -17,8 +18,40 @@ interface Props {
     placeData: Region[];
 };
 
-const Cards : React.FC<Props> = ({ placeData }) => {
-    console.log(placeData);
+const Cards: React.FC<Props> = ({ placeData }) => {
+  const [isLoading, setIsLoading] = useState(true);
+  useEffect(() => {
+        if (placeData.length > 0) {
+          setIsLoading(false);
+          // placeData가 로드되면 로딩 상태를 false로 설정
+        }
+  }, [placeData]);
+
+  if (isLoading) {
+        // 로딩 상태일 때 로딩 스켈레톤을 렌더링
+        return (
+          <div>
+            <div className={styles.container}>
+              {
+                placeData.map((place) => (
+                  <div key={place.id}>
+                    {
+                      place.attractions.map((attraction, index) => (
+                        <div className={styles.skelton}>
+                          <div className={styles['skelton__box']}>
+                            <div key={index}>{attraction.name}</div>
+                            <div key={index}>{attraction.type}</div>
+                          </div>
+                        </div>
+                    ))}
+                  </div>
+                ))
+              }
+            </div>
+          </div>
+        );
+    }
+
 
   return (
     <div>
@@ -35,7 +68,7 @@ const Cards : React.FC<Props> = ({ placeData }) => {
                       </div>
                     </div>
                 ))}
-              </div>
+            </div>
           ))
         }
       </div>
