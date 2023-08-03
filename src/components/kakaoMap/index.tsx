56e75557ca2b0fcfe23fd11526c898e234753f9a
김혜준 <alice5855@naver.com>
2023-08-03 20:35:36 +0900

    feat: 카카오맵 마커표시

--- src/components/kakaoMap/index.tsx
+++ src/components/kakaoMap/index.tsx
@@ -8,6 +8,25 @@ declare global {
 }
 
 const KakaoMap: React.FC = () => {
+  const positions = [
+    {
+      title: "뱅기",
+      latlng: { lat: 37.450705, lng: 126.570677 },
+    },
+    {
+      title: "숙소",
+      latlng: { lat: 37.450936, lng: 127.569477 },
+    },
+    {
+      title: "놀곳",
+      latlng: { lat: 36.450879, lng: 126.96994 },
+    },
+    {
+      title: "놀곳2",
+      latlng: { lat: 33.451393, lng: 126.570738 },
+    },
+  ];
+
   useEffect(() => {
     const container = document.getElementById('map');
     const options = {
@@ -16,7 +35,43 @@ const KakaoMap: React.FC = () => {
     };
 
     const map = new window.kakao.maps.Map(container, options);
-  }, []);
+
+    positions.forEach((position, index) => {
+      const markerPosition = new window.kakao.maps.LatLng(position.latlng.lat, position.latlng.lng);
+      const marker = new window.kakao.maps.Marker({
+        position: markerPosition,
+        image: new window.kakao.maps.MarkerImage(
+          "https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/markerStar.png",
+          new window.kakao.maps.Size(24, 35)
+        ),
+      });
+
+    const content = `<div style="padding:5px; font-weight:bold;">${index + 1}</div>`;
+
+    const customOverlay = new window.kakao.maps.CustomOverlay({
+      content: content,
+      position: markerPosition,
+      yAnchor: 1
+    });
+
+    const linePath = positions.map((position) => 
+      new window.kakao.maps.LatLng(position.latlng.lat, position.latlng.lng)
+    );
+      
+    const polylineOptions = {
+      path: linePath,
+      strokeWeight: 2,      
+      strokeColor: 'black',
+      strokeOpacity: 1,
+      strokeStyle: 'solid'
+    }; 
+    const polyline = new window.kakao.maps.Polyline(polylineOptions);
+      
+    polyline.setMap(map);
+    marker.setMap(map);
+    customOverlay.setMap(map); 
+  });
+  }, [positions]);
 
   return (
     <div className={styles.map}>
