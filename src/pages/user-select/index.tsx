d81a607455eb436f7a8c286610eb793059f8e953
KimMurPin <50792467+daepan@users.noreply.github.com>
2023-08-04 21:55:05 +0900

    Feature/select date time (#13)
    * feat: 여행 시간 설정 페이지 추가
    
    * feat: 주소 검색 API 세팅 및 기능 구현
    
    * feat: 시간 선정 관련 로직 생성
    
    * feat: JSON stringfy 제거
    
    * feat: 일정 시간 설정 기능 추가
    
    * feat: XML to JSON 함수 추가

--- src/pages/user-select/index.tsx
+++ src/pages/user-select/index.tsx
@@ -55,7 +55,7 @@ export default function UserSelect() {
       />
       <SelectProgressBar /> 
       {/* 전역변수로 프로그래스바 조정 */}
-      <Outlet context={[userSelectData, setUserSelectData]} />
+      <Outlet context={[useUserSelectData, useUserSelectData]} />
     </div>
   )
 }
