013b854fdb78ac3d124627f4a031f4badf7d2e5f
murpin <qw04011@gmail.com>
2023-08-04 21:54:50 +0900

    feat: XML to JSON 함수 추가

--- src/pages/Map/index.tsx
+++ src/pages/Map/index.tsx
@@ -1,12 +1,25 @@
 import React from "react";
 import KakaoMap from "components/kakaoMap";
+import {xmlToJSON} from "utils/xmlToJSON";
 import * as api from 'api';
 
 const useMap = () => {
-  React.useEffect(()=> {
-    const data = api.plan.getLocation();
-    console.log(data);
-  })
+  React.useEffect(() => {
+    const getLocationData = async () => {
+      const data = await api.plan.getLocation();
+      console.log(data);
+      // 예제 실행
+    xmlToJSON(data)
+      .then((jsonData: any) => {
+        console.log(jsonData);
+    })
+      .catch((error: any) => {
+      console.error(error);
+    });
+
+    }
+    getLocationData();
+  });
   return (
     <div>
       <KakaoMap />
