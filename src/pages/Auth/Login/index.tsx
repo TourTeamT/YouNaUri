3efc959ab2b0577d04e2ebb38afb9badc7e6dd82
김혜준 <alice5855@naver.com>
2023-08-07 02:13:36 +0900

    feat: 토큰 저장

--- src/pages/Auth/Login/index.tsx
+++ src/pages/Auth/Login/index.tsx
@@ -1,8 +1,28 @@
 import styles from './Login.module.scss';
 import { ReactComponent as Kakao } from 'assets/svg/auth/kakao.svg';
-import { KAKAO_REDIRECT_URL } from 'config/constants';
+import { useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
+import axios from 'axios';
 
 export default function Login(): JSX.Element {
+  const location = useLocation();
+
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const code = urlParams.get('code');
+
+    if (code) {
+      axios.get('http://localhost:3000', { params: { code: code } })
+        .then(function (response) {
+          console.log(response);
+          console.log("나와라쫌");
+          localStorage.setItem('access_token', response.data.access_token);
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+        }
+  }, [location]);
 
   return (
     <div className={styles.body}>
@@ -11,7 +31,7 @@ export default function Login(): JSX.Element {
           <span className={styles['template__title']}>LOGIN</span>
           <span className={styles['template__subTitle']}>로그인하기</span>
           <span className={styles['template__text']}>'저희 서비스'를 이용하려면 로그인해주세요.</span>
-          <a className={styles['template__kakao']} href={KAKAO_REDIRECT_URL}>
+          <a className={styles['template__kakao']} href={'http://3.37.87.24:3000/kakao-login/login'}>
             <Kakao/>
             <div className={styles['template__kakaoText']}>카카오 로그인</div>
           </a>
