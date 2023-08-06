8bfc634db42068abd7294fa1508827e2210acbfd
murpin <qw04011@gmail.com>
2023-08-06 19:29:53 +0900

    배포용

--- src/api/index.ts
+++
@@ -1,1 +0,0 @@
-export * as plan from './plan';
--- src/custom.d.ts
+++
@@ -1,6 +0,0 @@
-declare module "*.svg" {
-  import { FunctionComponent, SVGProps } from 'react';
-  export const ReactComponent: FunctionComponent<SVGProps<SVGSVGElement>>;
-  const src: string;
-  export default src;
-}
